0efabbd58b15e2c1c116a9c636e1d2a133a96893
Baubet <xavier.baubet@gmail.com>
2024-11-13 08:58:33 +0100

    Item

---
+++ src/Net.java
@@ -0,0 +1,6 @@
+public class Net extends Item{
+
+    public Net(String name){
+        super(name);
+    }
+}
---
+++ src/Sword.java
@@ -0,0 +1,11 @@
+public class Sword extends Item {
+
+    int Damage;
+
+    public Sword(String name, int damage) {
+        super(name);
+        this.Damage = damage;
+    }
+
+
+}
